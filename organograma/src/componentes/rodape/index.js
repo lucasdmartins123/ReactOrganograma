3caaf5625f62640ff60be267769932ac6643beed
Lucas Martins <lucasdallmartims@gmail.com>
2023-10-30 16:24:05 -0300

    first commit

---
+++ organograma/src/componentes/rodape/index.js
@@ -0,0 +1,38 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return(
+        <footer className ="footer">
+            <section>
+                <ul>
+                    <li>
+                        <a href="facebook.com" target="_blank">
+                            <img src="/imagens/fb.png" alt="facebook logo"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="twitter.com" target="_blank">
+                            <img src="/imagens/tw.png" alt="twitter logo"/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="Instagram.com" target="_blank">
+                            <img src="/imagens/ig.png" alt="instagram logo"/>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+            <section>
+                <img src="/imagens/logo.png" alt="logo"/>
+            </section>
+            <section>
+                <p>
+                    Desenvolvido por Lucas.
+                </p>
+            </section>
+        </footer>
+        
+    )
+}
+
+export default Rodape
